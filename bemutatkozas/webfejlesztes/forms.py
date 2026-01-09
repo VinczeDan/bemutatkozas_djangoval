ca465468584b9d111c18777597b18f84c59a9163
VinczeDan <vincze.daniel.22f@szlgbp.hu>
2026-01-09 15:51:12 +0100

    fe

--- bemutatkozas/webfejlesztes/forms.py
+++ bemutatkozas/webfejlesztes/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Név', max_length=100, widget=forms.TextInput(attrs={
+    name = forms.CharField(label='Név/ Name', max_length=100, widget=forms.TextInput(attrs={
         'placeholder': 'Név',
         'required': 'required'
     }))
@@ -9,7 +9,7 @@ class ContactForm(forms.Form):
         'placeholder': 'E-mail',
         'required': 'required'
     }))
-    message = forms.CharField(label='Üzenet', widget=forms.Textarea(attrs={
+    message = forms.CharField(label='Üzenet/ Message', widget=forms.Textarea(attrs={
         'placeholder': 'Üzenet',
         'required': 'required',
         'rows': 5
